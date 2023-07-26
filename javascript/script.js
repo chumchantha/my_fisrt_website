bfe8fc83201407efbf2e31896cb8f852a7baf3eb
chumchantha <chumchantha19y@gmail.com>
2023-07-26 13:35:11 +0700

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     const date = new Date();
     const hour = date.getHours();
-    console.log(hour);
     let message = '';
     if(hour <= 12){
         message = "Good Morning";
