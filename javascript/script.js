5f9c0578e8f92607f492ffdb4f28c937625b8115
chumchantha <chumchantha19y@gmail.com>
2023-07-26 13:34:43 +0700

    change h1 statement

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-    console.log("hello world");
+    const date = new Date();
+    const hour = date.getHours();
+    console.log(hour);
+    let message = '';
+    if(hour <= 12){
+        message = "Good Morning";
+    }
+    if(hour > 12){
+        message = "Good Afternoon"
+    }
+    if(hour >= 17){
+        message = "Good Evening"
+    }
+    $('#headingText').empty().append(message + " " + "From Chum Chantha");
 })
